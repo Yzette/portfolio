69b1d04212c19791d1c1cc4947a7c8e727ab8acf
alexa <alexa@localhost>
2021-10-09 17:42:42 +0200

    component project finished

--- my-portfolio/src/components/Experience/index.js
+++ my-portfolio/src/components/Experience/index.js
@@ -8,7 +8,8 @@ import japan from '../../assets/images/japan.jpg';
 import matrix from '../../assets/images/matrix.jpg';
 import back from '../../assets/icons/back.svg';
 import teaching from '../../assets/icons/school.svg';
-
+import nihon from '../../assets/icons/japan-flag.svg';
+import computer from '../../assets/icons/computer.svg';
 
 const Experience = () => (
     <div className="experience">
@@ -23,66 +24,77 @@ const Experience = () => (
                 <img className="experience_item_image" src={school} alt="image ecole"/>
                 <h2>L'enseignement</h2>
                 <img src={teaching} alt="icone ecole"/>
-                <h3>Qualités nécessaires</h3>
+                <h3>Savoir-être</h3>
                 <div className="experience_item_list_1">
                     <ul>
-                        <li>toto</li>
-                        <li>toto</li>
-                        <li>toto</li>
+                        <li>Empathie/ à l'écoute</li>
+                        <li>Pédagogue</li>
+                        <li>Patiente</li>
+                        <li>Adaptable</li>
+                        <li>Claire et concise</li>
                     </ul>
                 </div>
                 <h3>Tâches accomplies</h3>
                 <div className="experience_item_list_2">
                     <ul>
-                        <li>toto</li>
-                        <li>toto</li>
-                        <li>toto</li>
+                        <li>Préparation des cours</li>
+                        <li>Correction des copies</li>
+                        <li>Rédaction de compte rendu</li>
+                        <li>S'adapter au public en formation (classe, cours privé,formation adulte)</li>
                     </ul>
                 </div>
             
             </div>
             <div className="experience_line"></div>
             <div className="experience_item">
-                <img className="experience_item_image" src={school} alt="image ecole"/>
-                <h2>L'enseignement</h2>
-                <img src={teaching} alt="icone ecole"/>
-                <h3>Qualités nécessaires</h3>
+                <img className="experience_item_image" src={japan} alt="image ecole"/>
+                <h2>Japon</h2>
+                <img src={nihon} alt="icone ecole"/>
+                <h3>Savoir-être</h3>
                 <div className="experience_item_list_1">
                     <ul>
-                        <li>toto</li>
-                        <li>toto</li>
-                        <li>toto</li>
+                        <li>Bonne capacité d'adaptation</li>
+                        <li>Organisée</li>
+                        <li>Curiosité</li>
+                        <li>Sociable</li>
+                        <li>Bonne capacité d'apprentissage</li>
                     </ul>
                 </div>
                 <h3>Tâches accomplies</h3>
                 <div className="experience_item_list_2">
                     <ul>
-                        <li>toto</li>
-                        <li>toto</li>
-                        <li>toto</li>
+                        <li>Voyage dans différentes villes</li>
+                        <li>Apprentissage de la langue japonaise</li>
+                        <li>Serveuse dans un restaurant</li>
+                        <li>Vendeuse dans une boulangerie</li>
+                        <li>Travail dans une langue étrangère</li>
                     </ul>
                 </div>
             
             </div>
             <div className="experience_line"></div>
             <div className="experience_item">
-                <img className="experience_item_image" src={school} alt="image ecole"/>
-                <h2>L'enseignement</h2>
-                <img src={teaching} alt="icone ecole"/>
-                <h3>Qualités nécessaires</h3>
+                <img className="experience_item_image" src={matrix} alt="image ecole"/>
+                <h2>Le monde du numérique</h2>
+                <img src={computer} alt="icone ecole"/>
+                <h3>Savoir-être</h3>
                 <div className="experience_item_list_1">
                     <ul>
-                        <li>toto</li>
-                        <li>toto</li>
-                        <li>toto</li>
+                        <li>Sérieuse</li>
+                        <li>Rigoureuse</li>
+                        <li>Bonne capacité d'analyse</li>
+                        <li>Autonome</li>
+                        <li>Créative</li>
                     </ul>
                 </div>
                 <h3>Tâches accomplies</h3>
                 <div className="experience_item_list_2">
                     <ul>
-                        <li>toto</li>
-                        <li>toto</li>
-                        <li>toto</li>
+                        <li>Analyse et diagnostic de l'origine d'un problème</li>
+                        <li>Résolution à l'aide des developpeurs</li>
+                        <li>Rédaction d'une demande auprès des développeurs</li>
+                        <li>Tester, vérifier, explorer, inventorier les nouveautés d'un logiciel</li>
+                        <li>Présenter et expliquer le fonctionnement d'un logiciel</li>
                     </ul>
                 </div>
             
